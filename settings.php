4ff123ebe5a6f9482eebb771ccc23dc79353ee2e
Dan Attwood <dan.attwood@midkent.ac.uk>
2012-05-30 09:04:55 +0100

    Settings file as a stop gap to doing the settings properly in a database

---
+++ settings.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: DATTWOOD
+ * Date: 30/05/12
+ * Time: 08:42
+ * To change this template use File | Settings | File Templates.
+ */
+
+// Settings to be manually edited for now. Needs to be moved into an admin screen/ setting stable
+
+//The ID of the target reprots
+// Found by  select * from mdl_block_ilp_report
+$targetId = '3';
+
+
+// the ID of the reviews
+$reviewId = '3';
+
+?>
